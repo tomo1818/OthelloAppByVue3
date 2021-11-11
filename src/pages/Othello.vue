f88b3075bc263daaaf0f192508df8b70e89057c0
Aogu <kawakamiaogu@MacBook-Pro-77.local>
2021-11-11 15:09:28 +1100

    Rendering  stone

--- src/pages/Othello.vue
+++ src/pages/Othello.vue
@@ -13,19 +13,39 @@
       <div class="othelloContainer">
         <div class="stoneBox user1">
           <div class="box">
-            <div class="stone" v-for="(stone, index) in stone1" v-bind:key="index"></div>
+            <div
+              class="stone"
+              v-for="(stone, index) in stone1"
+              v-bind:key="index"
+            ></div>
           </div>
         </div>
         <table class="othelloTable">
           <tbody>
             <tr v-for="(value, rowNum, index) in table" v-bind:key="index">
-              <td v-for="(value2, columnNum, index2) in value" v-bind:key="index2">{{ value2 }}</td>
+              <td
+                v-for="(value2, columnNum, index2) in value"
+                v-bind:key="index2"
+              >
+                <div ref="root" class="stoneCon" v-if="value2 == 0">
+                  <i class="fas fa-circle fa-lg black front"></i>
+                  <i class="fas fa-circle fa-lg white back"></i>
+                </div>
+                <div ref="root" class="stoneCon" v-if="value2 == 1">
+                  <i class="fas fa-circle fa-lg white front"></i>
+                  <i class="fas fa-circle fa-lg black back"></i>
+                </div>
+              </td>
             </tr>
           </tbody>
         </table>
         <div class="stoneBox user2">
           <div class="box">
-            <div class="stone" v-for="(stone, index) in stone2" v-bind:key="index"></div>
+            <div
+              class="stone"
+              v-for="(stone, index) in stone2"
+              v-bind:key="index"
+            ></div>
           </div>
         </div>
       </div>
@@ -34,53 +54,67 @@
 </template>
 
 <script lang="ts">
-import {computed} from "vue"
-import { useStore } from 'vuex'
-import { key } from '../store'
+import { ref, computed, onMounted } from "vue";
+import { useStore } from "vuex";
+import { key } from "../store";
 
 export default {
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  setup () {
-    const store = useStore(key)
+  setup() {
+    const store = useStore(key);
+
+    /* 
+    const divs = ref([])
+    onMounted(() => {
+        console.log(divs.value) // [li, li ,li]
+    }) */
+
+    //const flip = () => {el.classList.toggle("flipped")}
 
     return {
       // state を呼び出す場合
-      count: computed(()=>store.state.count),
-      table: computed(()=>store.state.table), // オセロ盤の状態
-      stone1: computed(()=>store.state.stone1), // user1の残りの石
-      stone2: computed(()=>store.state.stone2), // user2の残りの石
+      count: computed(() => store.state.count),
+      table: computed(() => store.state.table), // オセロ盤の状態
+      stone1: computed(() => store.state.stone1), // user1の残りの石
+      stone2: computed(() => store.state.stone2), // user2の残りの石
       // mutation を呼び出す場合
-      increment:() =>store.commit("increment"),
+      increment: () => store.commit("increment"),
       // storeからの受け渡し確認用
-      showTable:(() => {
+      showTable: () => {
         console.log(store.state.table);
-      })
-    }
-  }
-}
+      },
+      /* flip: function() => {
+        console.log(this.$refs.card);
+        console.log(this.$refs.card.classList);
+        this.$refs.card.classList.toggle("flipped");
+        要素.classList.toggole("flipped");
+      } */
+    };
+  },
+};
 </script>
 
 <style scoped>
 table.othelloTable {
-    background:      #090;
-    border:          solid 2px #000;
-    border-collapse: collapse;
-    border-spacing:  0px;
-    table-layout:    fixed;
-    max-width: 960px;
-    margin: 0 auto;
+  background: #090;
+  border: solid 2px #000;
+  border-collapse: collapse;
+  border-spacing: 0px;
+  table-layout: fixed;
+  max-width: 960px;
+  margin: 0 auto;
 }
 
 table.othelloTable tr td {
-    width:           54px;
-    height:          54px;
-    border:solid 1px #fff;
-    text-align:      center;
-    vertical-align:  middle;
-    font-size:       200%;
-    box-sizing: content-box;
+  width: 54px;
+  height: 54px;
+  border: solid 1px #fff;
+  text-align: center;
+  vertical-align: middle;
+  font-size: 200%;
+  box-sizing: content-box;
 }
-table.othelloTable tr:first-child td{
+table.othelloTable tr:first-child td {
   border-top: none;
 }
 .stoneBox {
@@ -103,7 +137,7 @@ table.othelloTable tr:first-child td{
   display: flex;
   flex-wrap: wrap;
 }
-.stoneBox.user1 .box{
+.stoneBox.user1 .box {
   justify-content: flex-end;
 }
 .stoneBox.user2 .box {
@@ -113,5 +147,40 @@ table.othelloTable tr:first-child td{
   width: 11.47px;
   height: 100%;
   background: linear-gradient(90deg, #fff 0%, #fff 50%, #000 50%, #000 100%);
+}
+
+.black {
+  color: #000;
+}
+
+.white {
+  color: white;
+}
+
+/* ここから石をひっくり返すcss */
+.front,
+.back {
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  backface-visibility: hidden;
+  width: 100%;
+  height: 100%;
+}
+
+.stoneCon {
+  transition: transform 2s;
+  transform-style: preserve-3d;
+  cursor: pointer;
+}
+
+.stoneCon .back {
+  transform: rotateY(180deg);
+}
+/* クラスflippedを石の要素にtoggleしたらひっくり返せます */
+.stoneCon.flipped {
+  transform: rotateY(180deg);
 }
 </style>
