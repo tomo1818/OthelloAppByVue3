1afbd72b67515061a1e3da9fbec1e9897eb99188
Aogu <kawakamiaogu@MacBook-Pro-77.local>
2021-11-20 01:44:01 +1100

    merged file

--- src/types/type.ts
+++ src/types/type.ts
@@ -2,6 +2,7 @@ export interface Table {
   table: { [key: number]: { [key: number]: number | null } };
   stone1: number[];
   stone2: number[];
+  aroundStone: { y: number; x: number }[];
 }
 
 export interface State extends Table {
