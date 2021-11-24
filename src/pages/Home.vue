ae8479c8ea412e95ead7e7c47ffe69fa8e6ba5aa
hashiz008 <hashiz0314@icloud.com>
2021-11-24 11:59:13 +0900

    homePageImprovement 修正2

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,68 +1,51 @@
 <template>
-  <div class="backImg pt-10">
-  <div class="othelloTitleDiv">
-    <h1 class="othelloTitle">Othello</h1>
-  </div>
-  <div class="startBtn">
-    <router-link
-      class="p-3 btn hover:bg-gray-100 hover:bg-opacity-25 text-white rounded-full border-gray-100 hover:border-gray-100 shadow-3xl"
-      :to="{name:'Othello',
-      params: { mode: opponent, strength: difficulty },
-      params: { mode: opponent, name1: playerName1, name2: playerName2 }
-      }"
-      >スタート</router-link>
-     </div>
-
-     <div class="settingBtn mt-5">
-        <router-link class="p-3 hover:bg-gray-100 hover:bg-opacity-25 btn border-gray-100 hover:border-gray-100 text-white rounded-full shadow-3xl" to="/setting"
+  <div class="backImg pt-10 vh-100">
+    <div class="othelloTitleDiv">
+      <h1 class="othelloTitle">Othello</h1>
+    </div>
+    <div class="startBtn">
+      <router-link
+        class="p-3 btn hover:bg-gray-100 hover:bg-opacity-25 text-white rounded-full border-gray-100 hover:border-gray-100 shadow-3xl"
+        :to="{name:'Othello',
+        params: { mode: opponent, strength: difficulty },
+        params: { mode: opponent, name1: playerName1, name2: playerName2 }
+        }"
+        >スタート</router-link>
+    </div>
+    <div class="settingBtn mt-5">
+      <router-link class="p-3 hover:bg-gray-100 hover:bg-opacity-25 btn border-gray-100 hover:border-gray-100 text-white rounded-full shadow-3xl" to="/setting"
       >ゲーム設定</router-link
     >
-  </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital@1&family=M+PLUS+2:wght@700&display=swap');
-    .backImg {
-      background-image:url("../assets/othello.png");
-      width:100%;
-      height:825px;
-      padding-top:200px;
-    }
+  @import url('https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital@1&family=M+PLUS+2:wght@700&display=swap');
+  .backImg {
+    background-image:url("../assets/othello.png");
+    background-position: center center;
+    width:100%;
+    height:100%;
+    padding-top:200px;
+  }
 
-    .othelloTitleDiv {
-      font-size:80px;
-      margin-top:-100px;
-    }
+  .othelloTitleDiv {
+    font-size:80px;
+    margin-top:-100px;
+  }
 
-    .othelloTitle {
-      color:rgb(0, 0, 0);
-      font-family: 'Libre Baskerville', serif;
-    }
+  .othelloTitle {
+    color:rgb(0, 0, 0);
+    font-family: 'Libre Baskerville', serif;
+  }
 
-    .settingBtn {
-      margin-top: 50px;
-    }
+  .settingBtn {
+    margin-right:10px;
+  }
 
-    .startBtn {
-      margin-top: 150px;
-    }
+  .startBtn {
+    margin-top: 160px;
+    margin-right:20px;
+  }
 </style>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
