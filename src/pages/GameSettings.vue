e24cb3c58ef2de6e3763842ebe855289083c61cf
hashiz008 <hashiz0314@icloud.com>
2021-11-27 17:06:04 +0900

    設定ページ 全体的に修正

--- src/pages/GameSettings.vue
+++ src/pages/GameSettings.vue
@@ -1,56 +1,72 @@
 <template>
-  <div class="gemeSettings backColor pt-7">
+  <div class="gemeSettings backColor pt-6 vh-100">
     <div class="container">
-      <h1 class="title mb-3 text-3xl">設定</h1>
-      <div class="choseOpponent mb-5">
-        <h3 class="h5">対戦形式を選択してください</h3>
+      <h1 class="title mb-3 text-4xl">設定</h1>
+      <div class="choseOpponent mb-4">
+        <h3 class="h5 mb-4">対戦形式を選択してください</h3>
         <input
           type="radio"
           value="vsPlayer"
           id="vsPlayer"
-          class="form-radio text-pink-600"
+          class="text-pink-600 mr-2 radioColor1"
           v-model="setting.opponent"
           checked
           @change="changeOpponent"
         />
-        <label for="vsPlayer">vs Player</label>
+        <label for="vsPlayer" class="mr-5">vs Player</label>
         <input
           type="radio"
           value="vsCpu"
           id="vsCpu"
-          class="form-radio text-green-600"
+          class="text-green-600 mr-2"
           v-model="setting.opponent"
           @change="changeOpponent"
         />
         <label for="vsCpu">vs CPU</label>
       </div>
       <div
-        class="inputPlayerName mb-3"
+        class="inputPlayerName mb-4"
         :class="{ displayNone: setting.chosePlayer }"
       >
-        <h3 class="h3">プレイヤー名を入力してください</h3>
-        <div class="mb-2">
-          <input
-            class="bg-gray-200 border-2 rounded"
-            v-model="setting.playerName1"
-            placeholder="Palyer name 1"
-          />
-          <span>Player Name 1: {{ setting.playerName1 }}</span>
+        <h3 class="h5">プレイヤー名を入力してください</h3>
+        <div class="mb-3 mt-3">
+          <span class="mr-3">Player Name 1: </span>
+          <input class="
+              pl-4
+              leading-tight
+              shadow-xl
+              focus:outline-none focus:shadow-outline
+              text-gray-800
+              w-40
+              bg-gray-200
+              border-2
+              rounded
+            " v-model="setting.playerName1" placeholder="Palyer name 1" />
         </div>
-        <div class="mb-2">
+        <div>
+          <span class="mr-3 ">Player Name 2: </span>
           <input
-            class="bg-gray-200"
+            class="
+              shadow-xl
+              leading-tight
+              focus:outline-none focus:shadow-outline
+              border-2
+              text-gray-800
+              pl-4
+              w-40
+              bg-gray-200
+              rounded
+            "
             v-model="setting.playerName2"
-            placeholder="Palyer name 2"
+            placeholder="Player name 2"
           />
-          <span>Player Name 2: {{ setting.playerName2 }}</span>
         </div>
       </div>
       <div
         class="choseCpuStrength mb-3"
         :class="{ displayNone: setting.choseCpu }"
       >
-        <h3 class="h3">コンピュータの強さを選択してください</h3>
+        <h3 class="h5">コンピュータの強さを選択してください</h3>
         <input
           type="radio"
           value="easy"
@@ -58,31 +74,34 @@
           v-model="setting.difficulty"
           checked
         />
-        <label for="easy">Easy</label>
+        <label class="ml-2" for="easy">Easy</label>
         <input
+          class="ml-5"
           type="radio"
           value="normal"
           id="normal"
           v-model="setting.difficulty"
         />
-        <label for="normal">Normal</label>
+        <label class="ml-2" for="normal">Normal</label>
         <input
+          class="ml-5"
           type="radio"
           value="hard"
           id="hard"
           v-model="setting.difficulty"
         />
-        <label for="hard">Hard</label>
+        <label class="ml-2" for="hard">Hard</label>
       </div>
       <div
         class="determinFirstMove mb-3"
         :class="{ displayNone: setting.chosePlayer }"
       >
-        <h3 class="h3">先攻(黒石)のプレイヤーを決定します</h3>
+        <h3 class="h5 pb-1">先攻(黒石)のプレイヤーを決定します</h3>
         <input
           type="radio"
           value="player1"
           id="player1"
+          class="mr-2"
           v-model="setting.firstMove"
           @change="determineFirstMove"
           checked
@@ -90,8 +109,9 @@
         <label for="player1">Player1</label>
         <input
           type="radio"
-          value="player2"
+          value="Player2"
           id="player2"
+          class="ml-20 mr-1"
           v-model="setting.firstMove"
           @change="determineFirstMove"
         />
@@ -102,29 +122,31 @@
         class="determinFirstMove mb-3"
         :class="{ displayNone: setting.choseCpu }"
       >
-        <h3 class="h3">先攻(黒石)のプレイヤーを決定します</h3>
+        <h3 class="h5 mb-3">先攻(黒石)のプレイヤーを決定します</h3>
         <input
           type="radio"
           value="player1"
           id="player1"
+          class="mr-2"
           v-model="setting.firstMove"
           @change="determineFirstMove"
           checked
         />
-        <label for="player1">Player1</label>
+        <label class="mr-24" for="player1">Player1</label>
         <input
           type="radio"
           value="CPU"
           id="player2"
+          class="mr-2"
           v-model="setting.firstMove"
           @change="determineFirstMove"
         />
         <label for="player2">CPU</label>
-        <p>first move: {{ setting.firstMove }}</p>
+        <p class="mt-2">first move: {{ setting.firstMove }}</p>
       </div>
       <div class="selectColorTheme mb-3">
-        <h3 class="h3">オセロ版の色オプションを選んでください</h3>
-        <select v-model="setting.colorTheme">
+        <h3 class="h5">オセロ版の色オプションを選んでください</h3>
+        <select class="mt-2 text-gray-800 w-32 rounded-lg" v-model="setting.colorTheme">
           <option
             v-for="(option, key) in $store.state.colorCollections"
             :value="key"
@@ -134,18 +156,10 @@
           </option>
         </select>
       </div>
+      <!-- 良い書き方募集中です -->
       <router-link
-        v-if="setting.opponent == 'vsCpu'"
-        class="
-          p-3
-          btn
-          hover:text-white
-          text-red-500
-          hover:bg-red-500
-          rounded-full
-          border-red-500
-          shadow-xl
-        "
+        v-if="opponent == 'vsCpu'"
+        class="p-3 btn text-gray-700 rounded-full border-gray-500 shadow-2xl"
         :to="{
           name: 'Home',
           params: {
@@ -159,16 +173,7 @@
       <router-link
         disabled
         v-else
-        class="
-          p-3
-          btn
-          hover:text-white
-          text-red-500
-          hover:bg-red-500
-          rounded-full
-          border-red-500
-          shadow-xl
-        "
+        class="p-3 btn text-gray-800 rounded-full shadow-2xl"
         :to="{
           name: 'Home',
           params: {
@@ -198,8 +203,8 @@ export default {
     const store = useStore(key);
     const setting = reactive<SettingData>({
       opponent: 'vsPlayer',
-      playerName1: 'player1',
-      playerName2: 'player2',
+      playerName1: 'Player1',
+      playerName2: 'Player2',
       difficulty: 'easy',
       chosePlayer: false,
       choseCpu: true,
@@ -247,9 +252,9 @@ export default {
 }
 
 .backColor {
-  /* background-color:black; */
-  /* width: 100%;
-  height: 800px;
-  color:white */
+  background-color: gray;
+  width: 100%;
+  height: 100%;
+  color: white;
 }
 </style>
